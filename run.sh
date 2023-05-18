9e99814a9d3c593c3ea1325291f41ab95a4577e6
wangjiawei <wangjiawei@t3caic.com>
2023-05-18 17:17:45 +0800

    use local js

--- run.sh
+++ run.sh
@@ -9,5 +9,5 @@ nohup bash /smartview/scripts/smartview.sh &
 
 cd /smartview/client/
 nohup xvfb-run -a ./smartview.x86_64 -p $WS_PORT -screen-width 1920 -screen-height 1080 -signalingUrl ws://127.0.0.1:$STREAMPORT &
-cd /smartview_stream
+cd /stream
 ./webserver -t websocket -p $STREAMPORT
